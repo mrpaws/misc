a6b202b87f281c5c5c80f463146979d4cca64adb
paws <paws@delimitize.com>
2014-07-28 03:29:51 -0700

    switched to zsh and improved modularity

--- backup_mc.sh
+++ backup_mc.sh
@@ -1,19 +1,19 @@
-#!/bin/sh
+#!/usr/bin/zsh
 # backup_mc.sh - paws
-# Last Modified 07/21/2013
-
+# Last Modified 07/27/2014
 
 ## Change these to relative server locations,
+# TODO: MOdularize functoins, take input options, handle config file, allow parent dir OR specific weekly or monthly
 minecraft_home="/opt/MC"  # where the minecraft install is based. script won't run unless server.prroperties is here.
-backup_location="/storage/Backup/Minecraft/RoFCraft" # backup destination root
-
+backup_location="/opt/MCbackup_test"
+title=minecraft_server
 
 ## should probably leave these alone
 timestamp=`date +'%Y%m%d%H%M%S'`
 runtime=`date +%s`
 backdir="${backup_location}/incremental/${timestamp}"
-backname="${backdir}/minecraft_server.${timestamp}.cpio.gz"
-backup_loc_relative=`echo "${backup_location}" | awk -F\/ '{print $NF}'`
+backname="${backdir}/${title}.${timestamp}.cpio.gz"
+backup_loc_relative=$(basename "${backup_location}")
 daily_clean_flag="${backup_location}/.daily.stamp"
 weekly_clean_flag="${backup_location}/.weekly.stamp"
 daily_location="${backup_location}/daily"
@@ -22,17 +22,14 @@ incremental_location="${backup_location}/incremental"
 daily_comp=0
 weekly_comp=0
 
-
-
 ## backup management functions
 manage_backups() {
-
-last_daily=`check_flag "daily"`
+last_daily=$(check_flag "daily")
 # 86400 sec = 24 hr
 if [ `math "${last_daily}+86400"` -le ${runtime} ]; then
-	echo "  -Creating daily backup ..."
-	create_copy "daily"
-	daily_comp=1
+  echo "  -Creating daily backup ..."
+  create_copy "daily"
+  daily_comp=1
 fi
 
 if [ ${last_daily} -eq ${runtime} ]; then
@@ -42,10 +39,10 @@ fi
 
 # 604800 sec = 7 days
 last_weekly=`check_flag "weekly"`
-if [ `math "${last_weekly}+ 604800"` -le ${runtime} ]; then
-        echo "  -Creating weekly backup ..."
-        create_copy "weekly"
-	weekly_comp=1
+if [ $((${last_weekly}+604800)) -le ${runtime} ]; then 
+  echo "  -Creating weekly backup ..."
+  create_copy "weekly"
+  weekly_comp=1
 fi
 
 if [ ${last_weekly} -eq ${runtime} ]; then
@@ -58,27 +55,27 @@ roll_runtime
 echo "[INFO]: Incremental backups older than 24 hours have been removed."
 
 if [ ${daily_comp} -eq 1 ]; then
-        echo "[INFO]: Backup (daily) created at (${daily_location}/`echo ${backname} | awk -F\/ '{print $NF}'`)."
+  echo "[INFO]: Backup (daily) created at (${daily_location}/`echo ${backname} | awk -F\/ '{print $NF}'`)."
 fi
 if [ ${weekly_comp} -eq 1 ]; then
-	echo "[INFO]: Backup (weekly) created at (${weekly_location}/`echo ${backname} | awk -F\/ '{print $NF}'`)."
+  echo "[INFO]: Backup (weekly) created at (${weekly_location}/`echo ${backname} | awk -F\/ '{print $NF}'`)."
 fi
 
 }
 
 check_flag() {
 if [ "${*}" = "daily" ]; then
-	flagfile="${daily_clean_flag}"
-	dir="${daily_location}"
+  flagfile="${daily_clean_flag}"
+  dir="${daily_location}"
 elif [ "${*}" = "weekly" ]; then
-	flagfile="${weekly_clean_flag}"
-	dir="${weekly_location}"
+  flagfile="${weekly_clean_flag}"
+  dir="${weekly_location}"
 fi
 if [ ! -f "${flagfile}" ]; then
-	echo "${runtime}" > ${flagfile}
+  echo "${runtime}" > ${flagfile}
 fi
 if [ ! -d "${dir}" ]; then
-	mkdir -p "${dir}" 
+  mkdir -p "${dir}" 
 fi
 
 cat "${flagfile}"
@@ -86,29 +83,57 @@ cat "${flagfile}"
 
 create_copy() {
 if [ "${*}" = "daily" ]; then
-	newloc="${daily_location}"
+  newloc="${daily_location}"
 elif [ "${*}" = "weekly" ]; then
-	newloc="${weekly_location}"
+  newloc="${weekly_location}"
 fi
 cp -ipR ${backname} ${newloc}
 rval=$?
 if [ ${rval} -ne 0 ]; then
-	echo "[ERROR]: Could not sync backup ("${*}"). (cp -ipR ${backname} ${newloc})."
-	exit 4
+  echo "[ERROR]: Could not sync backup ("${*}"). (cp -ipR ${backname} ${newloc})."
+  exit 4
 fi
 }
 
 ## whatever the cron is set to, everything older than 24 hours is gone
 roll_runtime() {
 if [ ! -d ${incremental_location} -o ${incremental_location} = "/" -o ${incremental_location} = "" ]; then
-	echo "[ERROR]: Unable to safely remove incremental files.  Script is broken."
-	exit 5
+  echo "[ERROR]: Unable to safely remove incremental files.  Script is broken."
+  exit 5
 fi
 find ${incremental_location}/* -type d -prune -mtime +1 -exec rm -r {} \;
 rval=$?
 if [ ${rval} -ne 0 ]; then
-	echo "[ERROR]: Unable to clean up incremental backup files."
-	exit 6
+  echo "[ERROR]: Unable to clean up incremental backup files."
+  exit 6
+fi
+}
+
+check_back_root() {
+if [ ! -d ${backup_location} ]; then
+  echo "[WARN]: -Backup directory (${backup_location}) does not exist. Creating."
+  out=$(mkdir -p ${backup_location}) || echo "FAILED: $out." && exit 3
+fi
+}
+
+create_back_dir() {
+echo "  -Creating backup dir (${backdir})."
+mkdir -p ${backdir}
+rval=$?
+if [ ${rval} != 0 ]; then
+  echo "[ERROR]: Could not create backup directory (${backdir}). Exiting."
+  exit 1
+fi
+}
+
+do_backup() {
+echo "  -Backing up Minecraft server (${minecraft_home}) ..."
+cd ${minecraft_home}
+find . | grep -v "./${backup_loc_relative}" | cpio --quiet -o | gzip -c - > ${backname}
+rval=$?
+if [ ${rval} -ne 0 ]; then
+  echo "[ERROR]: Backup exited with return value (${rval}).  FAILED."
+  exit 2
 fi
 }
 
@@ -119,38 +144,11 @@ echo "${*}" | bc
 
 ## main Main MAIN
 
-echo "Backing up minecraft (${minecraft_home}) ..."
-if [ ! -f "${minecraft_home}/server.properties" ]; then
-	echo "[ERROR]: Cannot find server.properties in specified Minecraft root directory."
-	exit 3
-fi
-if [ ! -d ${backup_location} ]; then
-        echo "  -Backup directory (${backup_location}) does not exist. Creating."
-	mkdir -p ${backup_location}
-fi
-
-echo "  -Creating backup dir (${backdir})."
-mkdir -p ${backdir}
-rval=$?
-if [ ${rval} != 0 ]; then
-  	echo "[ERROR]: Could not create backup directory (${backdir}). Exiting."
-  	exit 1
-fi
-
-echo "  -Backing up Minecraft server (${minecraft_home}) ..."
-cd ${minecraft_home}
-find . | grep -v "./${backup_loc_relative}" | cpio --quiet -o | gzip -c - > ${backname}
-rval=$?
-if [ ${rval} -ne 0 ]; then
-	echo "[ERROR]: Backup exited with return value (${rval}).  FAILED."
-	exit 2
-fi
-
+check_back_root
+create_back_dir
+do_backup
 manage_backups 
 
 echo "[INFO}: Incremental backup created at (${backname})."
 echo "Normal Termination."
 exit 0
-
-
-
